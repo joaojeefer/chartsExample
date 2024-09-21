5ccda86f0240ca120f009ace1489507a3c2940a4
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-21 19:57:29 -0300

    refactor: :recycle: update home screen

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -2,127 +2,41 @@ import React from 'react';
 import {Text, View, ScrollView} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {styles} from './styles';
-import {AboutNavigationProp, RootRoutes} from '../../routes/types';
+import {HomeNavigationProp, RootRoutes} from '../../routes/types';
 
-import {HorizontalBarChart, LineChart} from 'react-native-charts-wrapper';
-import {Table, TableWrapper, Col} from 'react-native-table-component';
-
-import {Products, Transactions, Users} from '../../database';
+import { PointsByProductChart, UserHistoryPointsChart } from '../../components';
 
 export const HomeScreen = () => {
-  const navigation = useNavigation<AboutNavigationProp>();
+
+  const navigation = useNavigation<HomeNavigationProp>();
 
   function navigateTo(
-    screen: RootRoutes.ABOUT | RootRoutes.PRODUCTS | RootRoutes.USERS,
+    screen: RootRoutes.ABOUT | RootRoutes.PRODUCTS | RootRoutes.TRANSACTIONS | RootRoutes.USERS,
   ) {
     navigation.navigate(screen);
   }
 
   return (
     <View style={styles.container}>
-      <Text onPress={() => navigateTo(RootRoutes.ABOUT)} style={styles.menu}>
-        Sobre
-      </Text>
-      <Text onPress={() => navigateTo(RootRoutes.PRODUCTS)} style={styles.menu}>
-        Produtos
-      </Text>
-      <Text onPress={() => navigateTo(RootRoutes.USERS)} style={styles.menu}>
-        Usuários
-      </Text>
-
-      <View>
-        <ScrollView>
-          <LineChart
-            style={styles.chart}
-            data={{
-              dataSets: [{values: [{y: 1}, {y: 2}, {y: 1}]}],
-            }}
-          />
-          <HorizontalBarChart
-            style={styles.chart}
-            data={{
-              dataSets: [{values: [{y: 1}, {y: 2}, {y: 1}]}],
-            }}
-          />
-
-          <Table
-            borderStyle={{borderWidth: 3, borderColor: '#fff'}}
-            style={styles.table}>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={['teste']} textStyle={styles.titleTable} />
-              <Col data={['teste teste']} textStyle={styles.textTable} />
-            </TableWrapper>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={['teste']} textStyle={styles.titleTable} />
-              <Col data={['teste teste']} textStyle={styles.textTable} />
-            </TableWrapper>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={['teste']} textStyle={styles.titleTable} />
-              <Col data={['teste teste']} textStyle={styles.textTable} />
-            </TableWrapper>
-          </Table>
-
-          {Products.map(item => {
-            return (
-              <View
-                style={{
-                  flex: 1,
-                  borderBottomColor: 'red',
-                  borderBottomWidth: 2,
-                  marginHorizontal: 12,
-                  justifyContent: 'center',
-                }}
-                key={item.id}>
-                <Text
-                  numberOfLines={1}
-                  style={{fontSize: 24, fontWeight: '400'}}>
-                  {item.nome}
-                </Text>
-              </View>
-            );
-          })}
-
-          {Transactions.map(item => {
-            return (
-              <View
-                style={{
-                  flex: 1,
-                  borderBottomColor: 'blue',
-                  borderBottomWidth: 2,
-                  marginHorizontal: 12,
-                  justifyContent: 'center',
-                }}
-                key={item.id}>
-                <Text
-                  numberOfLines={1}
-                  style={{fontSize: 24, fontWeight: '400'}}>
-                  {item.data}
-                </Text>
-              </View>
-            );
-          })}
-
-          {Users.map(item => {
-            return (
-              <View
-                style={{
-                  flex: 1,
-                  borderBottomColor: 'yellow',
-                  borderBottomWidth: 2,
-                  marginHorizontal: 12,
-                  justifyContent: 'center',
-                }}
-                key={item.id}>
-                <Text
-                  numberOfLines={1}
-                  style={{fontSize: 24, fontWeight: '400'}}>
-                  {item.nome}
-                </Text>
-              </View>
-            );
-          })}
-        </ScrollView>
+      <View style={styles.nav}>
+        <Text onPress={() => navigateTo(RootRoutes.USERS)} style={styles.menu}>
+          USUÁRIOS
+        </Text>
+        <Text onPress={() => navigateTo(RootRoutes.PRODUCTS)} style={styles.menu}>
+          PRODUTOS
+        </Text>
+        <Text onPress={() => navigateTo(RootRoutes.TRANSACTIONS)} style={styles.menu}>
+          TRANSAÇÕES
+        </Text>
+        <Text onPress={() => navigateTo(RootRoutes.ABOUT)} style={styles.menu}>
+          SOBRE
+        </Text>
       </View>
+
+      <ScrollView contentContainerStyle={styles.scrolledContainer}>
+        <UserHistoryPointsChart />
+        <PointsByProductChart />
+      </ScrollView>
     </View>
   );
 };
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -7,30 +7,18 @@ export const styles = StyleSheet.create({
     paddingTop: metrics.spacing.small,
     paddingHorizontal: metrics.spacing.small,
   },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: metrics.spacing.small,
+  },
   menu: {
-    fontSize: metrics.font_size.small,
+    fontSize: metrics.font_size.x_small,
     fontWeight: 'bold',
-    textAlign: 'right',
     color: colors.secondary.dark,
     marginBottom: metrics.spacing.xx_small,
   },
-  chart: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    height: 200,
-    //width: '90%',
-  },
-
-  table: {
-    backgroundColor: '#ECECEC',
-    borderRadius: 15,
-  },
-  titleTable: {
-    textAlign: 'right',
-    color: '#9C9B9B',
-  },
-  textTable: {
-    textAlign: 'center',
-    color: '#9C9B9B',
+  scrolledContainer: {
+    flexGrow: 1,
   },
 });
