35e429be163a1091952ad4bfc581bc1f2654cd46
João Jeefer da Silva Félix <ter_jfelix@arezzo.com.br>
2024-09-21 16:52:38 -0300

    refactor: :recycle: update users screen layout

--- src/screens/users/index.tsx
+++ src/screens/users/index.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { styles } from './styles';
 
 import { Users } from '../../database/users';
 
 export const UsersScreen = () => {
   return (
-    <View>
-      <ScrollView>
-        {Users.map((item) => {
-          return (
-            <View style={{ flex: 1, borderBottomColor: 'yellow', borderBottomWidth: 2, marginHorizontal: 12, justifyContent: 'center' }} key={item.id}>
-              <Text numberOfLines={1} style={{ fontSize: 24, fontWeight: '400' }}>{item.nome}</Text>
-            </View>
-          )
-        })}
+      <ScrollView contentContainerStyle={styles.container}>
+        {Users.map((item) => <Text style={styles.item} key={item.id}>{item.nome}</Text>)}
       </ScrollView>
-    </View>
   );
 };
